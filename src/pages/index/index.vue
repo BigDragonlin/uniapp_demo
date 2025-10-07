99787d7daaaedda7647c39cde846bafc5a0c29fc
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-10-07 17:37:24 +0800

    安装测试 uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,9 +3,15 @@
     <swiper-item v-for="item in list" :key="item.id">
       <view>
         <image :src="item.url" @click="handleClick(item.url)"></image>
-      </view> 
+      </view>
     </swiper-item>
   </swiper>
+
+  <uni-card title="基础卡片" extra="额外信息">
+    <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+  </uni-card>
+
+
 </template>
 
 <script setup lang="ts">
