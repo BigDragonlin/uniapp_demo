31457b6f7d97143f0c37f971d029b0255f3f8103
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-10-07 15:31:05 +0800

    完成轮播图开发。

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,40 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <!-- <u-action-sheet :actions="list" :title="title" :show="show"></u-action-sheet> -->
-  </view>
+  <swiper class="banner-swiper" indicator-dots indicator-color="#999" indicator-active-color="#fff" autoplay>
+    <swiper-item v-for="item in list" :key="item.id">
+      <view>
+        <image :src="item.url" @click="handleClick(item.url)"></image>
+      </view> 
+    </swiper-item>
+  </swiper>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello,  uniapp')
 
+const list = ref([
+  { id: '1', url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_1.jpg" },
+  { id: '2', url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_2.jpg" },
+  { id: '3', url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_3.jpg" },
+  { id: '4', url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_4.jpg" },
+  { id: '5', url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_5.jpg" },
+])
+
+const handleClick = (url: string) => {
+  uni.previewImage({
+    urls: list.value.map(item => item.url),
+    current: url
+  })
+}
 </script>
 
 <style>
+.banner-swiper,
+.banner-swiper image {
+  width: 750rpx;
+  height: 750rpx;
+}
+
 .content {
   display: flex;
   flex-direction: column;
